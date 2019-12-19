eb92fd8f88f3ea9d3758a0f9594f28fc467e7d4b
innaanikina <avril-avril@mail.ru>
2019-12-19 14:51:08 +0500

    style fixes in food

--- src/main/java/commands/cook/Food.java
+++ src/main/java/commands/cook/Food.java
@@ -13,13 +13,10 @@ public class Food {
     public String language;
     public String description;
 
-    public List<Pair<Integer, String>> recipeSteps = new ArrayList<Pair<Integer, String>>(); //тут пары типа : (15, "посолите воду")
-                                                                             //где 15 минут от base time
+    public List<Pair<Integer, String>> recipeSteps = new ArrayList<Pair<Integer, String>>();
     public String ingredients;
 
     public static Set<String> tags = new HashSet<>();
-
-    //так надо будет составить и добавить в органайзер рецепт
 
     public Food(String name, String language) throws IOException {
         this.description = "";
@@ -31,10 +28,5 @@ public class Food {
             this.recipeSteps = food.recipeSteps;
             ingredients = food.ingredients;
         }
-        /*if (recipeSteps == null || ingredients == null){
-            RecipeInitializer.initRecipe(this);
-        }*/
     }
-
-    //public ArrayList<string> ingredients;
 }
